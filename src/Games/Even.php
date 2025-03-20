50d4e1140bd0b5a4d3a15175a1d4eb91e66850dc
MiranaM <0resama@mail.ru>
2025-03-20 16:48:05 +0300

    Added Brain Even

---
+++ src/Games/Even.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function BrainGames\Engine\runGame;
+
+function getEvenGameData(): array
+{
+    $question = rand(1, 100);
+    $correctAnswer = ($question % 2 === 0) ? 'yes' : 'no';
+    return [$question, $correctAnswer];
+}
+
+function runBrainEven()
+{
+    $description = 'Answer "yes" if the number is even, otherwise answer "no".';
+    runGame($description, fn() => getEvenGameData());
+}
