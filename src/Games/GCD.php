69dc062895776200ab905ddc512307e9c4a1dd9c
MiranaM <0resama@mail.ru>
2025-03-24 11:53:26 +0300

    GCD

---
+++ src/Games/GCD.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrainGames\Games;
+
+use function BrainGames\Engine\runGame;
+
+const GCD_DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+function gcd(int $a, int $b): int
+{
+    return $b === 0 ? $a : gcd($b, $a % $b);
+}
+
+function getQuestionAnswer(): array
+{
+    $num1 = rand(1, 100);
+    $num2 = rand(1, 100);
+    $question = "$num1 $num2";
+    $correctAnswer = (string) gcd($num1, $num2);
+
+    return [$question, $correctAnswer];
+}
+
+function playGcdGame(): void
+{
+    runGame(GCD_DESCRIPTION, fn() => getQuestionAnswer());
+}
